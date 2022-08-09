386e76f52288a8b26bc3aa160b2115fd3188b13f
Emblogs <Emzy45cool@gmail.com>
2022-08-08 22:07:43 -0700

    moddyfyed

--- Strings-methods.js
+++ Strings-methods.js
@@ -7,4 +7,24 @@ else{
     console.log("Incorrect")
 }
 
+
+// slice
 let food=["rice","beans","pizza","pap"]
+food.slice()
+food.slice()
+console.log(food)
+
+// touppercase
+let red="hello world"
+let dare=red.toUpperCase();
+console.log(dare)
+
+// tolowercase
+let deer="GOOD MORNING"
+let dear=deer.toLowerCase();
+console.log(dear)
+
+// trim
+let guo="    Free Time!   "
+let free=guo.trim();
+console.log(free)
